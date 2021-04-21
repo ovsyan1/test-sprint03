70b7026bc030fecb6453f93d84d09e8263c960eb
Kyrylo <77015424+ovsyan1@users.noreply.github.com>
2021-04-21 20:10:52 +0300

    css+html

---
+++ t02_upgrade_human/js/script.js
@@ -0,0 +1,23 @@
+class Human{
+    constructor(firstName, lastName, gender, age, calories, ){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.calories = calories;
+    }
+    sleepFor(){
+        return `I'm sleeping`
+    }
+    feed(){
+        return `Nom Nom Nom`
+    }
+}
+class Superhero{
+    fly(){
+        return `I'm flying`
+    }
+    fightWithEvil(){
+        return `Khhhh-chh... Bang-g-g-g... Evil is defeated!`
+    }
+}
