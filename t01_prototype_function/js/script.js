2f2dde5abddada3855b37237331cb5a63b27d215
Kyrylo <77015424+ovsyan1@users.noreply.github.com>
2021-04-21 17:03:15 +0300

    full task

--- t01_prototype_function/js/script.js
+++ t01_prototype_function/js/script.js
@@ -1,24 +1,20 @@
-
-
-
-String.prototype = removeDuplicates()
-
-
-let str = "Giant Spiders?   What's next? Giant Snakes?"
-
-console.log(str)
-let arr = str.split(' ');
-let newStr = [...new Set(arr)]
-console.log(newStr.join(' '))
- str =  ". . . . ? . . . . . . . . . . . ";
-console.log(str)
-//  arr = str.split(' ');
-//  newStr = [...new Set(arr)]
-str = str.removeDuplicates()
-console.log(str)
-
-function removeDuplicates(){
+function removeDuplicates() {
     let arr = str.split(' ');
     let newStr = [...new Set(arr)]
     return newStr.join(' ')
 }
+
+String.prototype.removeDuplicates = removeDuplicates;
+
+let str = "Giant Spiders?   What's next? Giant Snakes?"
+console.log(str)
+// Giant Spiders?   What's next? Giant Snakes?
+str = str.removeDuplicates();
+console.log(str)
+// Giant Spiders? What's next? Snakes?
+str =  ". . . . ? . . . . . . . . . . . ";
+console.log(str)
+// . . . . ? . . . . . . . . . . . 
+str = str.removeDuplicates()
+console.log(str);
+// .?
