ef3a4fa5d4ced501eb262a98ac437feef1134006
Kyrylo <77015424+ovsyan1@users.noreply.github.com>
2021-04-22 19:52:14 +0300

    return one line

--- t01_prototype_function/js/script.js
+++ t01_prototype_function/js/script.js
@@ -1,10 +1,12 @@
-function removeDuplicates() {
-    let arr = str.split(' ');
-    let newStr = [...new Set(arr)]
-    return newStr.join(' ')
-}
+// function removeDuplicates() {
+//     let arr = str.split(' ');
+//     let newStr = [...new Set(arr)]
+//     return newStr.join(' ')
+// }
 
-String.prototype.removeDuplicates = removeDuplicates;
+String.prototype.removeDuplicates = function() {
+    return [...new Set(this.split(' '))].join(' ')
+}
 
 let str = "Giant Spiders?   What's next? Giant Snakes?"
 console.log(str)
